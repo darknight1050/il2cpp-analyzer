06700d83fc591ce0b3cff58a9ebdbb9b9dd9532d
darknight1050 <darkdeveloping1050@gmail.com>
2022-07-10 17:44:23 +0200

    Add address and buildId search

--- analyzer.js
+++ analyzer.js
@@ -1,4 +1,4 @@
-module.exports = (json, addr) => {
+module.exports.analyzeSingle = (json, addr) => {
     let functions = json.functions;
     for (let i = 0; i < functions.length; i++) {
         let func = functions[i];
@@ -9,4 +9,27 @@ module.exports = (json, addr) => {
                 return func;
         }
     }
+    return 0;
+}
+
+module.exports.analyzeMultiple = (json, toAnalyze) => {
+    let analyzed = {};
+    let functions = json.functions;
+    for (let i = 0; i < functions.length; i++) {
+        let func = functions[i];
+        let ranges = func.ranges;
+        for (let j = 0; j < ranges.length; j++) {
+            let range = ranges[j];
+            let result = toAnalyze.filter(addr => parseInt(range[0], 16) <= addr && parseInt(range[1], 16) >= addr);
+            if(result.length > 0) {
+                result.forEach(addr => {
+                    toAnalyze.splice(toAnalyze.indexOf(addr), 1);
+                    analyzed[addr] = func;
+                });
+                if(toAnalyze.length <= 0)
+                    return analyzed;
+            }
+        }
+    }
+    return analyzed;
 }
--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,7 @@
+const { Console } = require("console");
+const { find } = require("lodash");
 const _ = require("lodash"),
-    analyze = require("../analyzer"),
+    { analyzeSingle, analyzeMultiple } = require("../analyzer"),
     fs = require("fs"),
     axios = require('axios');
 
@@ -12,6 +14,15 @@ String.prototype.splice = function(idx, replace, str) {
 };
 
 const versionsPath = "./versions/";
+const buildIDs = JSON.parse(fs.readFileSync(versionsPath + "BuildID.json"));
+
+versionFromBuildID = (buildID) => {
+    for (let version in buildIDs) {
+        if(buildIDs[version].includes(buildID))
+            return version;
+    }
+    return;
+}
 
 module.exports = (app) => {
     app.get("/api/versions", (req, res) => {
@@ -24,6 +35,60 @@ module.exports = (app) => {
         });
     });
 
+    app.get("/api/buildids", (req, res) => {
+        res.status(200).json(buildIDs);
+    });
+
+    app.post("/api/analyze/address", async (req, res) => {
+        if (_.isEmpty(req.body)) {
+            res.status(400).json({
+                success: false,
+                error: "No body!"
+            });
+            return;
+        }
+
+        let version = req.body.version.replace(/\.\./g, "").replace(/\//g, "").replace(/\\/g, "");
+        if (_.isEmpty(version)) {
+            res.status(400).json({
+                success: false,
+                error: "No version!"
+            });
+            return;
+        }
+        
+        let addresses = req.body.addresses;
+        let hasAddresses = !_.isEmpty(addresses);
+        if (!hasAddresses) {
+            res.status(400).json({
+                success: false,
+                error: "No addresses!"
+            });
+            return;
+        }
+        
+        if(ver = versionFromBuildID(version))
+            version = ver;
+
+        let fileName = versionsPath + version + ".json";
+        if (!fs.existsSync(fileName)) {
+            console.log(versionFromBuildID(version));
+            res.status(400).json({
+                success: false,
+                error: "Version not found!"
+            });
+            return;
+        }
+
+        let json = JSON.parse(fs.readFileSync(fileName));
+
+        res.status(200).json({
+            success: true,
+            version: version,
+            result: analyzeMultiple(json, addresses)
+        });
+    });
+
     app.post("/api/analyze", async (req, res) => {
         if (_.isEmpty(req.body)) {
             res.status(400).json({
@@ -32,9 +97,8 @@ module.exports = (app) => {
             });
             return;
         }
+
         let version = req.body.version.replace(/\.\./g, "").replace(/\//g, "").replace(/\\/g, "");
-        let stacktrace = req.body.stacktrace;
-        let url = req.body.url;
         if (_.isEmpty(version)) {
             res.status(400).json({
                 success: false,
@@ -42,6 +106,9 @@ module.exports = (app) => {
             });
             return;
         }
+        
+        let stacktrace = req.body.stacktrace;
+        let url = req.body.url;
         let hasStacktrace = !_.isEmpty(stacktrace);
         let hasUrl = !_.isEmpty(url);
         if (!hasStacktrace && !hasUrl) {
@@ -51,16 +118,20 @@ module.exports = (app) => {
             });
             return;
         }
-        let fileName = versionsPath + version + ".json";
-        if (!fs.existsSync(fileName)) {
-            res.status(400).json({
-                success: false,
-                error: "Version not found!"
-            });
-            return;
-        }
         
-        let json = JSON.parse(fs.readFileSync(fileName));
+        let json;
+        if(version != "BuildID") {
+            let fileName = versionsPath + version + ".json";
+            if (!fs.existsSync(fileName)) {
+                res.status(400).json({
+                    success: false,
+                    error: "Version not found!"
+                });
+                return;
+            }
+            json = JSON.parse(fs.readFileSync(fileName));
+        }
+
         const regexAddressPc = /(?<=pc ).+?(?=  \/data\/app\/com.beatgames.beatsaber-.+?\/lib\/arm64\/libil2cpp.so)/g;
         const regexAddressAt = /(?<=at libil2cpp\.).+?(?=\(Native Method\))/g;
         let analyzedStackTrace = "";
@@ -70,11 +141,13 @@ module.exports = (app) => {
             try {
                 let res = await axios.get(url);
                 analyzedStackTrace = res.data;
-                const regexAndroidRuntime = /E AndroidRuntime:/;
-                const regexCrash = /E CRASH   :/; 
-                let lines = analyzedStackTrace.split('\n').filter(line => regexAndroidRuntime.test(line) || regexCrash.test(line));
-                analyzedStackTrace = "";
-                lines.forEach(line => analyzedStackTrace += line + "\n");
+                if(!analyzedStackTrace.startsWith("*** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***")) {
+                    const regexAndroidRuntime = /E AndroidRuntime:/;
+                    const regexCrash = /E CRASH   :/; 
+                    let lines = analyzedStackTrace.split('\n').filter(line => regexAndroidRuntime.test(line) || regexCrash.test(line));
+                    analyzedStackTrace = "";
+                    lines.forEach(line => analyzedStackTrace += line + "\n");
+                }
             } catch(err) {
                 res.status(400).json({
                     success: false,
@@ -87,7 +160,28 @@ module.exports = (app) => {
         let result;
         while(result = regexAddressPc.exec(analyzedStackTrace)) {
             let addr = parseInt(result.toString(), 16);
-            let analyzed = analyze(json, addr);
+            if(!json) {
+                const search = "(BuildId: ";
+                let indexStart = analyzedStackTrace.indexOf(search, result.index);
+                let indexEnd = analyzedStackTrace.indexOf(")", indexStart);
+                if(!(version = versionFromBuildID(analyzedStackTrace.substring(indexStart + search.length, indexEnd)))) {
+                    res.status(400).json({
+                        success: false,
+                        error: "Version not found!"
+                    });
+                    return;
+                }
+                let fileName = versionsPath + version + ".json";
+                if (!fs.existsSync(fileName)) {
+                    res.status(400).json({
+                        success: false,
+                        error: "Version not found!"
+                    });
+                    return;
+                }
+                json = JSON.parse(fs.readFileSync(fileName));
+            }
+            let analyzed = analyzeSingle(json, addr);
             if(analyzed) {
                 let startAddr = analyzed.ranges.sort((a, b) => a[0] - b[0])[0][0];
                 const search = "libil2cpp.so";
@@ -95,10 +189,11 @@ module.exports = (app) => {
                 analyzedStackTrace = analyzedStackTrace.insert(index + search.length, " (" + analyzed.sig + "+" + (addr-startAddr) + ")");
             }
         }
+
         while(result = regexAddressAt.exec(analyzedStackTrace)) {
             let addrStr = result.toString();
             let addr = parseInt(addrStr, 16);
-            let analyzed = analyze(json, addr);
+            let analyzed = analyzeSingle(json, addr);
             if(analyzed) {
                 let startAddr = analyzed.ranges.sort((a, b) => a[0] - b[0])[0][0];
                 let indexEnd = analyzed.sig.indexOf("(");
