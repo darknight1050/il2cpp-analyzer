99879f52e534d6bee743b27bde96f7edcc38ede7
darknight1050 <darkdeveloping1050@gmail.com>
2023-03-07 10:07:58 +0100

    Use await for qpackagesDownloader

--- qpackagesDownloader.js
+++ qpackagesDownloader.js
@@ -34,67 +34,73 @@ const getDownloadedPackages = () => {
 }
 
 const downloadPackages = async () => {
-    await axios.get(baseUrl)
-        .then(res => {
-            const downloadedPackages = getDownloadedPackages();
-            const packages = res.data;
-            console.log(packages);
-            for(let package of packages) {
-                const downloadedPackage = downloadedPackages.find(downloadedPackage => downloadedPackage.name == package);
-                axios.get(baseUrl + package + "?limit=0")
-                    .then(res => {
-                        const versions = res.data;
-                        for(let version of versions) {
-                            if(!downloadedPackage || !downloadedPackage.versions.includes(version.version)) {
-                                axios.get(baseUrl + package + "/" + version.version)
-                                    .then(res => {
-                                        const additionalData = res.data?.config?.info?.additionalData;
-                                        if(additionalData &&!additionalData.headersOnly && !additionalData.staticLinking) {
-                                            const debugSoLink = additionalData.debugSoLink;
-                                            if(debugSoLink) {
-                                                axios.get(debugSoLink, { responseType: "stream" })
-                                                    .then(res => {
-                                                        if (!fs.existsSync(versionsPath + "/" + package))
-                                                            fs.mkdirSync(versionsPath + "/" + package);
-                                                        const writer = fs.createWriteStream(versionsPath + "/" + package + "/" + version.version + ".so", res.data);
-                                                        return new Promise((resolve, reject) => {
-                                                            res.data.pipe(writer);
-                                                            let error = null;
-                                                            writer.on("error", err => {
-                                                                error = err;
-                                                                writer.close();
-                                                                reject(err);
-                                                            });
-                                                            writer.on("close", () => {
-                                                                if (!error) {
-                                                                    console.log("Downloaded " + package + " version " + version.version);
-                                                                    resolve(true);
-                                                                } else {
-                                                                    console.log("Error downloading " + package + " version " + version.version + ": " + error);
-                                                                }
-                                                            });
-                                                        });
-                                                    })
-                                                    .catch(e => {
-                                                        console.log(debugSoLink + ": Error: ", e.message);
-                                                    });
-                                            }
-                                        }
-                                    })
-                                    .catch(e => {
-                                        console.log(baseUrl + package + "/" + version.version + ": Error: ", e.message);
-                                    }); 
-                            }
-                        }
-                    })
-                    .catch(e => {
-                        console.log(baseUrl + package + "?limit=0" + ": Error: ", e.message);
-                    });
-            }
-        })
+    const downloadedPackages = getDownloadedPackages();
+    const packages = await axios.get(baseUrl)
+        .then(res => res.data)
         .catch(e => {
-            console.log(baseUrl + ": Error: ", e.message);
+            console.log(baseUrl + ": Error: " + e.message);
         });
+    console.log(packages);
+    if(packages) {
+        for(let package of packages) {
+            const downloadedPackage = downloadedPackages.find(downloadedPackage => downloadedPackage.name == package);
+            const versions = await axios.get(baseUrl + package + "?limit=0")
+                .then(res => res.data)
+                .catch(e => {
+                    console.log(baseUrl + package + "?limit=0" + ": Error: " + e.message);
+                });
+            if(versions) {
+                for(let version of versions) {
+                    if(!downloadedPackage || !downloadedPackage.versions.includes(version.version)) {
+                        const additionalData = await axios.get(baseUrl + package + "/" + version.version)
+                            .then(res => res.data?.config?.info?.additionalData)
+                            .catch(e => {
+                                console.log(baseUrl + package + "/" + version.version + ": Error: ", e.message);
+                            }); 
+                        if(additionalData) {
+                            if(!additionalData.headersOnly && !additionalData.staticLinking) {
+                                const debugSoLink = additionalData.debugSoLink;
+                                if(debugSoLink) {
+                                    axios.get(debugSoLink, { responseType: "stream" })
+                                        .then(res => {
+                                            if (!fs.existsSync(versionsPath + "/" + package))
+                                                fs.mkdirSync(versionsPath + "/" + package);
+                                            const writer = fs.createWriteStream(versionsPath + "/" + package + "/" + version.version + ".so", res.data);
+                                            return new Promise((resolve, reject) => {
+                                                res.data.pipe(writer);
+                                                let error = null;
+                                                writer.on("error", err => {
+                                                    error = err;
+                                                    writer.close();
+                                                    reject(err);
+                                                });
+                                                writer.on("close", () => {
+                                                    if (!error) {
+                                                        console.log("Downloaded " + package + " version " + version.version);
+                                                        resolve(true);
+                                                    } else {
+                                                        console.log("Error downloading " + package + " version " + version.version + ": " + error);
+                                                    }
+                                                });
+                                            });
+                                        })
+                                        .catch(e => {
+                                            console.log(debugSoLink + ": Error: " + e.message);
+                                        });
+                                }
+                            }
+                        } else {
+                            console.log("Couldn't get version " + version + " for " + package);
+                        }
+                    }
+                }
+            } else {
+                console.log("Couldn't get versions for " + package);
+            }
+        }
+    } else {
+        console.log("Couldn't get packages");
+    }
 }
 
 module.exports = { downloadPackages };
